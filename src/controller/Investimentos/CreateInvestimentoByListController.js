593c4754f7151c2d3a58cac85c4fab7097b276d8
Guilherme Araujo <guilherme.araujo14@hotmail.com>
2024-02-26 18:55:39 -0300

    alteração para fazer alguns testes

--- src/controller/Investimentos/CreateInvestimentoByListController.js
+++ src/controller/Investimentos/CreateInvestimentoByListController.js
@@ -3,25 +3,24 @@ const CreateInvestimentoByListModel = require('../../model/Investimentos/CreateI
 
 async function percorreLista(req, res){
     const dados = req.body;
-    let count = 0;
+    try {
+        //console.log(dados)
+        const data = JSON.parse(dados)
+        console.log(data)
+        data.map(dado =>{
+            console.log(dado)
+        })
+
+        return res.status(200).json({message: `Lista carregada com sucesso!`})
+    } catch (error) {
+        console.log(error)
+    }
+    /*let count = 0;
     dados.forEach((element) => {
         console.log(element) 
         count +=1
         return       
-    });
-return res.status(200).json({message: `Lista percorrida ${count} vezes com sucesso!`})
-}
-
-async function CreateInvestimentoByList(req, res){
-    const dados = req.body;
-    console.log(dados)
-    try {
-        const response = await CreateInvestimentoByListModel.percorreLista(dados)
-        return res.status(201).json(response)
-
-    } catch (error) {
-        return res.status(400).json(error)
-    }
+    });*/
 }
 
 module.exports = {
